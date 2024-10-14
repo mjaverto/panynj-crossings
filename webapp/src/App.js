5984eb79ef2c4f2fafa648132548c8d4476c6d76
Michael Averto <mjaverto@channelape.com>
2024-10-14 10:03:29 -0400

    Update for single crossing at a time

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect, useRef } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { Title, Grid, Card, LoadingOverlay, MantineProvider, Switch, Container } from '@mantine/core';
+import { Title, Grid, Card, LoadingOverlay, MantineProvider, Switch, Container, Select, Text, Tooltip } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/charts/styles.css';
 import { DateTimePicker } from '@mantine/dates';
 import { LineChart } from '@mantine/charts';
 import moment from 'moment-timezone';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 // Supabase client initialization 
 const supabase = createClient('https://jurzflavaojycfbqjyex.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1cnpmbGF2YW9qeWNmYnFqeWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2ODIxNzcsImV4cCI6MjA0MDI1ODE3N30.pzgMDzfizUDWa5pBrnNLklTKd2Gr-zhVnLWPuWO35fc');
@@ -17,24 +18,25 @@ function App() {
   const [chartData, setChartData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isAutoRefreshOn, setIsAutoRefreshOn] = useState(false);
+  const [selectedCrossing, setSelectedCrossing] = useState('Holland Tunnel');
+  const [crossings, setCrossings] = useState([]);
 
-  async function fetchData() {
+  async function fetchData(crossing) {
     setIsLoading(true);
 
     try {
       const adjustedDateTime = moment(selectedDateTimeRef.current).tz('America/New_York').utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
 
-      // Fetch data for all crossings at once
       const { data, error } = await supabase.rpc('get_crossing_data', {
-        in_time_stamp: adjustedDateTime,  // Pass the time_stamp filter
-        in_order_direction: 'DESC',  // Set the order direction (DESC or ASC)
-        in_limit: 10000,  // Set the limit
+        in_time_stamp: adjustedDateTime,
+        in_order_direction: 'DESC',
+        in_limit: 10000,
       });
 
       if (error) {
         console.error('Error fetching data from Supabase:', error);
       } else {
-        const formattedData = formatDataForMantineChart(data);
+        const formattedData = formatDataForMantineChart(crossing === 'All' ? data : data.filter(item => item.crossing_display_name === crossing));
         setChartData(formattedData);
       }
     } catch (error) {
@@ -51,8 +53,37 @@ function App() {
   }, [selectedDateTime]);
 
   useEffect(() => {
-    fetchData();
+    const fetchCrossings = async () => {
+      const { data, error } = await supabase
+        .from('facilities')
+        .select('crossing_display_name')
+        .order('crossing_display_name');
+
+      if (error) {
+        console.error('Error fetching crossings:', error);
+      } else {
+        const crossingNames = data.map(item => item.crossing_display_name);
+        setCrossings(crossingNames);
+        // No need to set selectedCrossing here as it's already set to 'Holland Tunnel'
+      }
+    };
+
+    fetchCrossings();
   }, []);
+
+  useEffect(() => {
+    fetchData(selectedCrossing);
+  }, [selectedCrossing, selectedDateTime]);
+
+  useEffect(() => {
+    if (isAutoRefreshOn) {
+      handleRefresh();
+      const intervalId = setInterval(handleRefresh, 60000);
+      return () => clearInterval(intervalId);
+    } else {
+      fetchData();
+    }
+  }, [isAutoRefreshOn, selectedDateTime]);
 
   const handleRefresh = () => {
     const currentTime = new Date();
@@ -63,19 +94,9 @@ function App() {
 
     if (formattedCurrentTime !== formattedSelectedTime) {
       setSelectedDateTime(currentTime);
-      fetchData();
+      fetchData(selectedCrossing);
     }
   };
-
-  useEffect(() => {
-    if (isAutoRefreshOn) {
-      handleRefresh();
-      const intervalId = setInterval(handleRefresh, 60000);
-      return () => clearInterval(intervalId);
-    }else { 
-      fetchData();
-    }
-  }, [isAutoRefreshOn, selectedDateTime]);
 
   function formatDataForMantineChart(data) {
     const formattedData = {};
@@ -168,6 +189,19 @@ function App() {
     return formattedData;
   }
 
+  const getCrossingOptions = () => {
+    const crossings = [
+      'All',
+      'Holland Tunnel',
+      'Lincoln Tunnel',
+      'Bayonne Bridge',
+      'George Washington Bridge',
+      'Goethals Bridge',
+      'Outerbridge Crossing'
+    ];
+    return crossings.map(crossing => ({ value: crossing, label: crossing }));
+  };
+
   return (
     <MantineProvider>
       <div>
@@ -177,7 +211,14 @@ function App() {
             <Grid.Col span={{ base: 12, md: 12, lg: 12 }}>
               <Card withBorder shadow="sm" p="lg">
                 <DateTimePicker
-                  label="Select Date and Time"
+                  label={
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                      Select Date and Time
+                      <Tooltip label="History starts from Sept 1, 2024" position="right" withArrow>
+                        <IconInfoCircle size={16} style={{ marginLeft: '5px', cursor: 'pointer' }} />
+                      </Tooltip>
+                    </div>
+                  }
                   placeholder="Select date and time"
                   value={selectedDateTime}
                   valueFormat="MMM DD YYYY hh:mm A"
@@ -199,67 +240,76 @@ function App() {
                   }}
                   mt="md"
                 />
+                <Select
+                  label="Select Crossing"
+                  placeholder="Choose a crossing"
+                  data={getCrossingOptions()}
+                  value={selectedCrossing}
+                  onChange={(value) => setSelectedCrossing(value)}
+                  mt="md"
+                />
               </Card>
             </Grid.Col>
 
             <Grid.Col span={{ base: 12, md: 12, lg: 12 }}>
-
               <LoadingOverlay visible={isLoading} />
-              {!isLoading && Object.keys(chartData).map((crossing, index) => (
-                <div key={index}>
-                  <Card withBorder shadow="sm" p="lg" mb={15}>
-                    <Title order={3} align="center">{crossing}</Title>
-                    <Grid gutter="md">
+              {!isLoading && Object.keys(chartData)
+                .filter(crossing => selectedCrossing === 'All' || crossing.startsWith(selectedCrossing))
+                .map((crossing, index) => (
+                  <div key={index}>
+                    <Card withBorder shadow="sm" p="lg" mb={15}>
+                      <Title order={3} align="center">{crossing}</Title>
+                      <Grid gutter="md">
 
-                      <Grid.Col span={{ base: 12, md: 6 }}>
-                        <h4 align="center">Westbound</h4>
-                        <LineChart
-                          data={chartData[crossing].westbound}
-                          h={300}
-                          dataKey="date"
-                          strokeWidth={5}
-                          curveType="natural"
-                          dotProps={{ r: 0 }}
-                          activeDotProps={{ r: 8 }}
-                          withLegend
-                          series={[
-                            { name: 'Route Speed', color: 'green.6' },
-                            { name: 'Route Travel Time', color: 'red.6' },
-                          ]}
-                          xAxisProps={{
-                            tickFormatter: (date) => moment(date, 'YYYY-MM-DD hh:mm A').format('M/D/YY h:mma'),
-                            angle: 0,
-                            minTickGap: 100,
-                          }}
-                        />
-                      </Grid.Col>
+                        <Grid.Col span={{ base: 12, md: 6 }}>
+                          <h4 align="center">Westbound</h4>
+                          <LineChart
+                            data={chartData[crossing].westbound}
+                            h={300}
+                            dataKey="date"
+                            strokeWidth={5}
+                            curveType="natural"
+                            dotProps={{ r: 0 }}
+                            activeDotProps={{ r: 8 }}
+                            withLegend
+                            series={[
+                              { name: 'Route Speed', color: 'green.6' },
+                              { name: 'Route Travel Time', color: 'red.6' },
+                            ]}
+                            xAxisProps={{
+                              tickFormatter: (date) => moment(date, 'YYYY-MM-DD hh:mm A').format('M/D/YY h:mma'),
+                              angle: 0,
+                              minTickGap: 100,
+                            }}
+                          />
+                        </Grid.Col>
 
-                      <Grid.Col span={{ base: 12, md: 6 }}>
-                        <h4 align="center">Eastbound</h4>
-                        <LineChart
-                          data={chartData[crossing].eastbound}
-                          h={300}
-                          dataKey="date"
-                          strokeWidth={5}
-                          curveType="natural"
-                          dotProps={{ r: 0 }}
-                          activeDotProps={{ r: 8 }}
-                          withLegend
-                          series={[
-                            { name: 'Route Speed', color: 'green.6' },
-                            { name: 'Route Travel Time', color: 'red.6' },
-                          ]}
-                          xAxisProps={{
-                            tickFormatter: (date) => moment(date, 'YYYY-MM-DD hh:mm A').format('M/D/YY h:mma'),
-                            angle: 0,
-                            minTickGap: 100,
-                          }}
-                        />
-                      </Grid.Col>
-                    </Grid>
-                  </Card>
-                </div>
-              ))}
+                        <Grid.Col span={{ base: 12, md: 6 }}>
+                          <h4 align="center">Eastbound</h4>
+                          <LineChart
+                            data={chartData[crossing].eastbound}
+                            h={300}
+                            dataKey="date"
+                            strokeWidth={5}
+                            curveType="natural"
+                            dotProps={{ r: 0 }}
+                            activeDotProps={{ r: 8 }}
+                            withLegend
+                            series={[
+                              { name: 'Route Speed', color: 'green.6' },
+                              { name: 'Route Travel Time', color: 'red.6' },
+                            ]}
+                            xAxisProps={{
+                              tickFormatter: (date) => moment(date, 'YYYY-MM-DD hh:mm A').format('M/D/YY h:mma'),
+                              angle: 0,
+                              minTickGap: 100,
+                            }}
+                          />
+                        </Grid.Col>
+                      </Grid>
+                    </Card>
+                  </div>
+                ))}
             </Grid.Col>
           </Grid>
         </Container>
